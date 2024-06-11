b19edd57e9b90dc1ede222726bbdf31f63223b68
Heladio Francisco Morales Dorantes <inghfrancisco.morales@gmail.com>
2024-06-11 00:17:17 -0600

    fix gps message

--- app/src/main/java/com/gomotorscompany/gomotors/enprodresodetail/view/progresodetail.java
+++ app/src/main/java/com/gomotorscompany/gomotors/enprodresodetail/view/progresodetail.java
@@ -258,7 +258,7 @@ public class progresodetail extends AppCompatActivity implements View.OnClickLis
             buttonenpointOrder2.setVisibility(View.VISIBLE);
             Glide.with(context).load(R.drawable.ic_goblack).into(imagesemaforo) ;
             buttonenpointOrder2.setText("Recolectar");/**  aceptar orden*/
-
+            button.setText("GPS NEGOCIO");
             viewrecolectar.setBackground(blackdot);
             viewEncola.setBackground(whitedot);
             viewenprogreso.setBackground(whitedot);
@@ -275,7 +275,8 @@ public class progresodetail extends AppCompatActivity implements View.OnClickLis
         }
         else if(semaforodelaorden==2)
         {
-            buttonenpointOrder2.setVisibility(View.VISIBLE);
+            button.setText("GPS NEGOCIO");
+            buttonenpointOrder2.setVisibility(View.VISIBLE);//todo
             Glide.with(context).load(R.drawable.ic_gogray).into(imagesemaforo) ;//ic_pizzastatus1recolectar
             buttonenpointOrder2.setText("Recolectado");//En cola
             viewrecolectar.setBackground(blackdot);
@@ -292,6 +293,7 @@ public class progresodetail extends AppCompatActivity implements View.OnClickLis
 
         }else if(semaforodelaorden==3)
         {
+            button.setText("GPS CLIENTE");
             buttonenpointOrder2.setVisibility(View.VISIBLE);
             Glide.with(context).load(R.drawable.ic_goyellow).into(imagesemaforo) ;//ic_pizzastatus2encola
             buttonenpointOrder2.setText("En camino");//en progreso
